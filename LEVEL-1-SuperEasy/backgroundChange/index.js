b011b044714e1e0ef54751f393ebd4aaa223319f
mayuri modi <mayurimodi368@gmail.com>
2025-08-06 12:45:00 +0530

    BackgroundChnage mini challenge

---
+++ LEVEL-1-SuperEasy/backgroundChange/index.js
@@ -0,0 +1,8 @@
+const btnEle = document.querySelector('.buttons button');
+const h1Ele = document.querySelector('h1');
+
+
+function changeBodyBg(color){
+document.body.style.background = color;
+h1Ele.style.color =' white';
+}
