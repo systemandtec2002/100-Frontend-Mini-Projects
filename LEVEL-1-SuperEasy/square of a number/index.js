5e860e85dbd714c87b54947309a1ebf92ed93cd8
mayuri modi <mayurimodi368@gmail.com>
2025-09-02 14:14:50 +0530

    square calculator

---
+++ LEVEL-1-SuperEasy/square of a number/index.js
@@ -0,0 +1,22 @@
+const input = document.getElementById("numberInput");
+ const button = document.getElementById("calBtn");
+  const result = document.getElementById("resultDisplay");
+
+  button.addEventListener("click",() =>{
+const value = Number(input.value.trim());
+
+    if(!isNaN(value)){
+ const square = value * value;
+ result.textContent = `Result: ${square}`;
+    }
+     else {
+        result.textContent = "Please enter a valid number.";
+        result.style.color = "#ef4444"; 
+      }
+  })
+    
+
+
+//   What is isNaN(value)?
+// isNaN() stands for "is Not a Number"
+
