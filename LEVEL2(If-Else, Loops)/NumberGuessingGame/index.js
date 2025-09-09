bf8f3bc74a7f22aac0fb11b7c60c1f7259aa7c62
mayuri modi <mayurimodi368@gmail.com>
2025-09-09 11:39:25 +0530

    passwordStrengthChecker

--- LEVEL2(If-Else, Loops)/NumberGuessingGame/index.js
+++ LEVEL2(If-Else, Loops)/NumberGuessingGame/index.js
@@ -1,47 +1,38 @@
 
-
-
-
-;(function a (){
+(function () {
   const userInput = document.getElementById('user-input');
+  const form = document.querySelector('form');
+  const result = document.querySelector('.result');
+  const userGuess = document.querySelector('.your-guesses');
+  const submitBtn = document.querySelector('.submit-btn');
+  const startGameBtn = document.querySelector('.start-game');
+  const allGuessArray = [];
+  let randomNumber = Math.round(Math.random() * 100 + 1);
 
-const form = document.querySelector('form');
-const result = document.querySelector('.result');
-const userGuess = document.querySelector('.your-guesses');
-const submitBtn =   document.querySelector('.submit-btn');
-const startGameBtn = document.querySelector('.start-game');
-const allGuessArray = [];
-let randomNumber = Math.round(Math.random()*100 + 1);
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const userInputValue = parseInt(userInput.value);
 
+    if (userInputValue < randomNumber) {
+      result.innerText = 'Too low!';
+    } else if (userInputValue > randomNumber) {
+      result.innerText = 'Too high!';
+    } else {
+      result.innerText = '🎉 Congrats! You guessed it!';
+      startGameBtn.disabled = false;
+      submitBtn.disabled = true;
+    }
 
-form.addEventListener("submit",(e) =>{
-e.preventDefault()
-  // console.log("submitted");
-  const userInputValue = parseInt(userInput.value);
-  if(userInputValue < randomNumber ){
-result.innerText = 'too low';
-  }else if(userInputValue > randomNumber){
-result.innerText = 'too high';
-  }else{
-    result.innerText = 'congrats';
-  startGameBtn.disabled = false;
-  submitBtn.disabled = true;
-  }
-  // userInput.value = ""; this is first method to empty the input value or we can do it form.reset()
-  allGuessArray.push(userInputValue);
-userGuess.innerText = 'Your guesses: ' + allGuessArray.join(', ');
-  form.reset();
-})
+    allGuessArray.push(userInputValue);
+    userGuess.innerText = 'Your guesses: ' + allGuessArray.join(', ');
+    form.reset();
+  });
 
-startGameBtn.addEventListener("click",() =>{
-  userGuess.innerText = "";
+  startGameBtn.addEventListener("click", () => {
+    userGuess.innerText = "";
     result.innerText = "";
     startGameBtn.disabled = true;
-  submitBtn.disabled =false;
- randomNumber = Math.round(Math.random() * 100 + 1);
-
-
-})
-
-})
-
+    submitBtn.disabled = false;
+    randomNumber = Math.round(Math.random() * 100 + 1);
+  });
+})();
