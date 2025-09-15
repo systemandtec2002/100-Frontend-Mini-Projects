3a3c8b9b7e4ec3444573d779d551d8325ce15503
mayuri modi <mayurimodi368@gmail.com>
2025-09-15 16:15:20 +0530

    passwordStrengthChecker

--- LEVEL2(If-Else, Loops)/passwordStrenghtChecker/index.js
+++ LEVEL2(If-Else, Loops)/passwordStrenghtChecker/index.js
@@ -1,26 +1,33 @@
-const password = document.getElementById('password');
-const message = document.getElementById('message');
-const str = document.getElementById('strength');
 
-password.addEventListener("input",() =>{
+const password = document.getElementById("password");
+const message  = document.getElementById("message");
+const strength  = document.getElementById("strength");
+
+
+
+ password.addEventListener("input",() =>{
   if(password.value.length > 0){
-    message.style.display = 'block'
-
+message.style.display = "block"
   }else{
-    message.style.display = 'block'
+    message.style.display = "none"
   }
+
   if(password.value.length < 4){
-str.innerHTML = 'weak'
-password.style.borderColor = "#ff5925";
-message.style.color = '#ff5925'; 
-  }else if(password.value.length >= 4 && password.value.length < 8){
-str.innerHTML = 'medium'
+strength.innerHTML = "weak";
+password.style.borderColor = "#ff5929";
+message.style.color = "#ff5929";
+
+  }else if(password.value.length > 4 && password.value.length < 8){
+strength.innerHTML = "medium";
+
 password.style.borderColor = "yellow";
-message.style.color = "yellow"; 
+message.style.color = "yellow";
 
   }else if(password.value.length >= 8){
-str.innerHTML = 'strong'
+    strength.innerHTML = "strong"
 password.style.borderColor = "#26d730";
-message.style.color = '#26d730'; 
+message.style.color = "#26d730";
+
+
   }
-})
+ })
